f75d53646740fd80a153033c7af1825ad40effa2
Invisible Acropolis <invisibleacropolis@gmail.com>
2026-03-30 18:43:35 -0400

    Publish InvisibleSupport2 route

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
             input: {
                 main: resolve(__dirname, 'index.html'),
                 invisibleSupport: resolve(__dirname, 'invisible-support/index.html'),
+                invisibleSupport2: resolve(__dirname, 'InvisibleSupport2/index.html'),
             },
         },
     },
